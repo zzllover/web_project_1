d51502f36eff45423555c6bf0ee39c7327446d39
peng <294407816@qq.com>
2019-04-08 16:17:19 +0800

    a

--- 天地无极乾坤逆转/7-千杯醉不倒leetcode/30-串联所有子串.js
+++ 天地无极乾坤逆转/7-千杯醉不倒leetcode/30-串联所有子串.js
@@ -1,2 +1,18 @@
 //Substring with Concatenation of All Words
 
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number[]}
+ */
+
+/*
+  Input:
+  s = "wordgoodgoodgoodbestword",
+  words = ["word","good","best","word"]
+  Output: []
+*/
+var findSubstring = function(s, words) {
+  let wn = words.length;
+  
+};
